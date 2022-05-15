384b55914f7496ec9a6a38687b6f0f06fd46f712
JuManji <opghld@gmail.com>
2022-05-15 14:13:30 +0300

    Update upd-ans-cron.sh

--- upd-ans-cron.sh
+++ upd-ans-cron.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ansible all -a "rm -r ironfish-autodeposit" -u root
 ansible all -a "git clone https://github.com/JuManjey/ironfish-autodeposit.git" -u root
 ansible all -a "chmod +x ironfish-autodeposit/autodeposit.py" -u root
 ansible all -a "systemctl enable cron" -u root
